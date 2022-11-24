d3add44210529488361de898e97896864c472e7e
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2022-11-24 13:40:48 +0200

    setting the pomodoro settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import SetPomodoro from "./components/SetPomodoro";
 
 function App() {
-  return <>hello world</>;
+  return (
+    <div className="container">
+      <h1> Pomodoro</h1>
+      <small>Be productive the right way.</small>
+      <SetPomodoro />
+    </div>
+  );
 }
 
 export default App;
